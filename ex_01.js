843f63746c32b94bf1bcfc58f95d3a0b9afde9b1
Myriam789 <myriam.sall@epitech.digital>
2024-10-02 10:30:08 +0200

    ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,13 @@
+function getAngryDog(numberOfWoofs) {
+
+for (let i= 0; i < 5; i = i +1) {
+console.log("woofwoofwoofwoofwoof");
+return("woofwoofwoofwoofwoof")
+
+}
+}
+
+
+//Some code to test your function output : (you can modify it but /!\ do not deliver it /!\ ). Only deliver your function
+var result = getAngryDog(3);
+displayResult(result);
